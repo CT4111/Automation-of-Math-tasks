453cbf0e7255ae1a79e052e16c3d3cd0ec7ec168
CT4111 <Gaber_Simon@gmx.de>
2023-09-01 00:00:38 +0200

    Update GausAlgorhytmus.py

--- GausAlgorhytmus.py
+++ GausAlgorhytmus.py
@@ -1,11 +1,4 @@
 import numpy as np
-n = 3
-numbers = np.zeros((n,n+1))
-length = len(numbers)*len(numbers[0])
-for i in range(len(numbers)):
-    for c in range(len(numbers[0])):
-        numbers[i][c]=float(input("input value"))
-
 def solve():
     for m in range(n-1):
         for i in range(n -1-m):
@@ -14,4 +7,16 @@ def solve():
             for v in range(n-m):
                 numbers[i + 1+m][v + 1] -= numbers[m][v + 1] * c
     print(numbers)
+
+n = float(input("amount of variables"))
+variabls = np.zeros((n))
+numbers = np.zeros((n,n+1))
+length = len(numbers)*len(numbers[0])
+for i in range(len(numbers)):
+    for c in range(len(numbers[0])):
+        numbers[i][c]=float(input("input value"))
+
 solve()
+def solve2():
+    for i in range(n-1):
+        c = numbers[i][i+1] / numbers[i+1][i+1]
