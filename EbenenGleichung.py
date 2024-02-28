ed8d862121c1967e30e4bcfdd7c8a22f030e199a
CT4111 <117856677+CT4111@users.noreply.github.com>
2024-02-28 23:05:47 +0100

    Update EbenenGleichung.py

--- EbenenGleichung.py
+++ EbenenGleichung.py
@@ -1,6 +1,4 @@
 import numpy as np
-def NormalenForm(vectors):
-    normalV = ([vectors[1][1]-vectors[2][2]],[vectors[1][2]-vectors[2][0]],[vectors[1][0]-vectors[2][1]])
 Punkte = np.zeros((3,3))
 vector = np.zeros((3,3))
 for i in range(3):
@@ -12,7 +10,6 @@ vector[0]=Punkte[0]
 print(Punkte)
 print(vector)
 print("{} + a * {} + b * {}".format(vector[0],vector[1],vector[2]))
-print(f"normalen Vektor = {NormalenForm(vector)}")
 #Ziel output
 #   x10 + a*x11 + b*x12
 #   x10 + a*x11 + b*x12
